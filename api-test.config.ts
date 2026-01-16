92c818ab2e8866681d34d6e70384af0456a1a816
Vitalii Ostrovyi <vostrovyi@gmail.com>
2026-01-16 20:43:27 +0200

    Env variables

--- api-test.config.ts
+++ api-test.config.ts
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+
 const processENV = process.env.TEST_ENV
-const env = processENV || 'dev'
+const env = processENV || 'prod'
 console.log('Test Environment is: ' + env)
 
 
@@ -14,8 +19,11 @@ if (env === 'qa') {
         config.userPassword = 'Qwerty@123'
 }
 if (env === 'prod') {
-    config.userEmail = '',
-        config.userPassword = ''
+    // if (!process.env.PROD_USERNAME || !process.env.PROD_PASSWORD) {
+    //     throw Error('Missing requered environment variables')
+    // }
+    config.userEmail = process.env.PROD_USERNAME as string,
+    config.userPassword = process.env.PROD_PASSWORD as string
 }
 
 export { config }
--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    trace:'retain-on-failure'
+    trace: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
