dba27fdac72c03e219791205080027349f1a1441
Vitalii Ostrovyi <vostrovyi@gmail.com>
2026-01-16 20:23:16 +0200

    Traces enabled on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {},
+  use: {
+    trace:'retain-on-failure'
+  },
 
   /* Configure projects for major browsers */
   projects: [
